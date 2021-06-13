6e59446a344a34a5432948fa7bb05753d77be53c
elit <elitmanes@gmail.com>
2021-06-13 17:22:43 +0300

    starting

--- src/components/FavoriteGames.vue
+++ src/components/FavoriteGames.vue
@@ -38,27 +38,27 @@ export default {
       ]
     };
   },
-  // methods: {
-  //   async updateGames(){
-  //     console.log("response");
-  //     try {
-  //       const response = await this.axios.get(
-  //         "http://localhost:3000/games/favoriteGames",
-  //       );
-  //       const games = response.data.games;
-  //       this.games = [];
-  //       this.games.push(...games);
-  //       console.log(response);
-  //     } catch (error) {
-  //       console.log("error in update games")
-  //       console.log(error);
-  //     }
-  //   }
-  // }, 
-  // mounted(){
-  //   console.log("favorite games mounted");
-  //   this.updateGames(); 
-  // }
+  methods: {
+    async updateGames(){
+      console.log("response");
+      try {
+        const response = await this.axios.get(
+          "http://localhost:3003/users/favoriteGames",
+        );
+        const games = response.data.games;
+        this.games = [];
+        this.games.push(...games);
+        console.log(response);
+      } catch (error) {
+        console.log("error in update games")
+        console.log(error);
+      }
+    }
+  }, 
+  mounted(){
+    console.log("favorite games mounted");
+    this.updateGames(); 
+  }
 };
 </script>
 
